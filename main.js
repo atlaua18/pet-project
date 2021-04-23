8e47710ad56fec90bb74554c456ba80b019f5d3e
atlaua18 <nastyaopen@yandex.ru>
2021-04-23 22:38:30 +0300

    add weather

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-/* Установите ширину боковой навигации 250 пикселей, и левый край содержимого страницы - 250 пикселей. */
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "250px";
-//     document.getElementById("main").style.marginLeft = "250px";
-//   }
-  
-  /* Установите ширину боковой навигации равной 0, и левое поле содержимого страницы - 0. */
-//   function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//     document.getElementById("main").style.marginLeft = "0";
-//   }
+import weather from "./weather.js";
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
     let menuIcon = document.querySelector('.burger_menu');
     menuIcon.addEventListener('click', () => {
         document.getElementById("mySidenav").classList.toggle('sidenav250');
---
+++ weather.js
@@ -0,0 +1,27 @@
+export default document.addEventListener('DOMContentLoaded', async () => {
+    const divDeg = document.querySelector('.temp');
+    const imgIcon = document.querySelector('.icon');
+
+    function getCelsius(obj) {
+        let celsius = Math.round(obj.main.temp - 273.15);
+        if(celsius > 0) {
+            divDeg.innerHTML = '+' + celsius + '&#8451;';
+        } else {
+            divDeg.innerHTML = Math.round(obj.main.temp - 273.15) + '&#8451;';
+        }
+    }
+
+    async function getWeather() {
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Leningrad&appid=879e1b0421202dc81be8d808ca9f75f1', {
+            method: 'GET',
+            heareds: {
+                'Content-Type': 'application/json',
+            }
+        });
+        response.json().then(obj => {
+            getCelsius(obj);
+            imgIcon.setAttribute('src', `http://openweathermap.org/img/wn/${obj.weather[0].icon}.png`);
+        });
+    }
+    await getWeather();
+});
